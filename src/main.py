f731d82f1035f6260b46563db138c2dd125dee45
spirin-vadim <vadis99@ya.ru>
2024-05-19 13:16:29 +0500

    create report with sums of actual flats by date and addresses

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import re
+import datetime
+
+raw_data = pd.read_csv("data.csv", sep="\t")
+data = raw_data[['description', 'actualized_at']]
+df = pd.DataFrame()
+
+
+def address_from_description(descr):
+    pat_prefix = r'\d-комнатная квартира на\s*'
+    pat_postfix = r'\s*\d*\s*подъезд'
+    address = re.sub(pat_prefix, '', descr)
+    address = re.sub(pat_postfix, '', address)
+    return address
+
+
+def timestamp_to_date(actualized_at):
+    to_datetime = datetime.datetime.fromisoformat(actualized_at)
+    formated = to_datetime.strftime("%d.%m.%Y")
+    return formated
+
+
+df["Корпус"] = data["description"].apply(address_from_description) # Получаем чистый адрес
+df["Дата"] = data["actualized_at"].apply(timestamp_to_date) # Получаем дату из дата-время
+df = df.groupby(['Дата', 'Корпус']).size().reset_index(name='Кол-во активных квартир') # Группируем данные и получаем суммы значений по парам дата-адрес
+df['Дата'] = pd.to_datetime(df['Дата'], format='%d.%m.%Y') # До этого момента дата была str, из-за чего сортировка работала бы соответствующе
+df = df.sort_values(by="Дата") # Сортируем по возрастанию даты
+df['Дата'] = df['Дата'].dt.strftime('%d.%m.%Y') # Форматируем обратно
+df.to_excel("result.xlsx", index=False) # Пишем dataframe в таблицу excel
+
